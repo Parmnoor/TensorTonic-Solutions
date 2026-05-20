5be91720736ae6eff3b717abbda443dbf4486d30
Parmnoor Dhaliwal <parmnoor.dhaliwal@gmail.com>
2026-05-20 19:36:14 -0400

    Add solution for sql-running-totals

---
+++ study-plans/sql-basics/sql-running-totals/sql-running-totals.sql
@@ -0,0 +1,11 @@
+-- Write your SQL query here
+SELECT 
+    account,
+    txn_date,
+    amount,
+    SUM(amount) OVER (PARTITION BY account ORDER BY txn_date ASC, id ASC) AS running_total
+FROM transactions
+ORDER BY 
+    account ASC,
+    txn_date ASC,
+    id ASC;
